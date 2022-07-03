6197e1eee4053e4aa31371c0e1da01aab751b984
AndyBoyee <n.andyvue@outlook.com>
2022-07-03 18:47:17 -0500

    readme gen done

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
 # README Generator
 
-## Purpose 
-Tired of typing out a README.md for all your projects? This will make your life a whole lot easier, just answer the questions and type in the information required and it will generate a README.md file with everything typed out for you!
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-None-blue)](https://shields.io)
 
-## Built With
-HTML | JS | Node | NPM
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
 
-## Usage
+  ## Description:
+  Tired of typing out a README.md for all your projects? This will make your life a whole lot easier, just answer the questions and type in the information required and it will generate a README.md file with everything typed out for you!
 
+  ## Installation:
+  Install npm inquirer for VS Code.
 
-## Source Referenced
-[Google](https://www.google.com) | 
-[MDN Web Docs](https://developer.mozilla.org/en-US) |
-[npm](https://www.npmjs.com/) |
-[]
+  ## Usage:
+  Open in VS Code, view terminal and type 'node index' to run. Then simply answer all questions provided and your README.md will be created for you.
+
+  ## License:
+  None
+
+  ## Contribution:
+  No contribution needed.
+
+  ## Testing:
+  Test in VS Code.
+  
+  ## Additional Info:
+  - Github: [AndyBoyee604](https://github.com/AndyBoyee604)
+  - Email: email@email.com 
--- index.js
+++ index.js
@@ -1,13 +1,168 @@
-// TODO: Include packages needed for this application
+// Packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./src/generateMarkdown.js')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Questions
+const questions = [
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  // Project name
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+    validate: titleInput => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log('Title required!');
+        return false;
+      }
+    }
+  },
 
-// TODO: Create a function to initialize app
-function init() {}
+  // Description
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Describe your project:',
+    validate: descriptionInput => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log('Description required!');
+        return false;
+      }
+    }
+  },
+
+  // Installation Instructions, Confirmation if there is an installation process or not
+  {
+    type: 'confirm',
+    name: 'confirmInstallation',
+    message: 'Do you need to install this project or is there any packages that needs installation for it to work?'
+  },
+  // If confirmed or not
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter instructions for installation:',
+    when: ({ confirmInstallation }) => {
+      if (confirmInstallation) {
+        return true;
+      }
+    }
+  },
+
+  // Usage Information
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this project?',
+    validate: usageInput => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log('Usage information required!');
+        return false;
+      }
+    }
+  },
+
+  // Contribution Guidlines
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'How to contribute to this project?',
+    validate: contributionInput => {
+      if (contributionInput) {
+        return true;
+      } else {
+        console.log('Contribution info required!');
+        return false;
+      }
+    }
+  },
+
+  // Test Instructions 
+  {
+    type: 'input',
+    name: 'testing',
+    message: 'How can someone test this project?',
+    validate: testingInput => {
+      if (testingInput) {
+        return true;
+      } else {
+        console.log('Testing instructions required!');
+        return false;
+      }
+    }
+  },
+
+  // License
+  {
+    type: 'checkbox',
+    name: 'licensing',
+    message: 'Choose your license',
+    choices: ['Apache', 'MIT', 'None'],
+    validate: licensingInput => {
+        if (licensingInput) {
+            return true;
+        } else {
+            console.log('License required!');
+            return false;
+        }
+    }
+},
+
+  // Github Username
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter GitHub username:',
+    validate: githubInput => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log('GitHub username required!');
+        return false;
+      }
+    }
+  },
+
+  // Email Address
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email:',
+    validate: emailInput => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log('Email required!');
+        return false;
+      }
+    }
+  },
+];
+
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err)
+      throw err;
+    console.log('Success! README.md created!')
+  });
+};
+
+// Function to initialize app
+function init() {
+  inquirer.prompt(questions)
+    .then(function (userInput) {
+      console.log(userInput)
+      writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();
